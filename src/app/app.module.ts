e677988c8d9e083da5dbbd1e690b42fd42dcaf16
M Gosschalk <monica.gosschalk@btinternet.com>
2017-12-03 21:52:32 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes }   from '@angular/router';
+import { ContentfulService } from './contentful.service';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
+// define the available routes
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products',  component: ProductListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ContentfulService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
